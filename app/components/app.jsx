260fdb162827d6c9af19fb199df7c57dd5bb933a
adammertel <mertel.adam@gmail.com>
2018-09-04 23:41:25 +0200

    react dummy component test

---
+++ app/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { observer, action } from 'mobx-react'
+import Base from './../base'
+
+export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+        dummy component test
+      </div>
+    )
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+import React from 'react'
+import { render } from 'react-dom'
+import DevTools from 'mobx-react-devtools'
+import App from './components/app'
+
+document.body.innerHTML = ''
+
+render(
+  <div>
+    {/*  {DEVELOPMENT && <DevTools />} */}
+    <App />
+  </div>,
+  document.body.appendChild(document.createElement('div'))
+)

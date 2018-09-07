0d56e9b40581e5c9c75ba86118004f0e95cf22a1
adammertel <mertel.adam@gmail.com>
2018-09-07 10:26:32 +0200

    map store

---
+++ app/stores/map.ts
@@ -0,0 +1,27 @@
+import { observable, action, computed } from 'mobx'
+import Base from './../helpers/base'
+
+export default class MapStore {
+  _center
+  _zoom
+  constructor(data) {
+    this._center = observable.box([0, 0])
+    this._zoom = observable.box(6)
+  }
+
+  @computed
+  get center(): Array<Number> {
+    return this._center.get()
+  }
+
+  @computed
+  get zoom(): Number {
+    return this._zoom.get()
+  }
+
+  @action
+  mapMoved(newCenter: Array<Number>, newZoom: Number): void {
+    this._center.set(newCenter)
+    this._zoom.set(newZoom)
+  }
+}

1c595ac3dbe59b790a01e91f7294204e39e742f1
adammertel <mertel.adam@gmail.com>
2018-09-19 14:52:30 +0200

    panel settings styling

--- app/components/panelsettings.tsx
+++ app/components/panelsettings.tsx
@@ -25,7 +25,7 @@ export default class PanelSettingsComponent extends React.Component<
     const position = this.props.position
     return (
       <div
-        className="panelfilter-wrapper panel-component panel-top-component"
+        className="panelsetting-wrapper panel-component panel-top-component"
         style={{
           position: 'absolute',
           top: position.y,
@@ -33,21 +33,23 @@ export default class PanelSettingsComponent extends React.Component<
           height: position.h
         }}
       >
-        <div className="heading">Timeline ordering</div>
-        <div className="new-filter">
-          <div className="select is-small">
-            <select
-              value={this.props.store.sortProp.id}
-              onChange={this.handleOrdedChange.bind(this)}
-            >
-              {this.props.store.timeBarOrdering.map(orderAtt => {
-                return (
-                  <option value={orderAtt.id} key={orderAtt.id}>
-                    {orderAtt.label}
-                  </option>
-                )
-              })}
-            </select>
+        <div className="settings-block">
+          <div className="heading">Timeline ordering</div>
+          <div className="new-filter">
+            <div className="select is-small">
+              <select
+                value={this.props.store.sortProp.id}
+                onChange={this.handleOrdedChange.bind(this)}
+              >
+                {this.props.store.timeBarOrdering.map(orderAtt => {
+                  return (
+                    <option value={orderAtt.id} key={orderAtt.id}>
+                      {orderAtt.label}
+                    </option>
+                  )
+                })}
+              </select>
+            </div>
           </div>
         </div>
       </div>

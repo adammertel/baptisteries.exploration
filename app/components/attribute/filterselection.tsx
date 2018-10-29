d94c0f809ebaeed520617e5d829fadc32be00ee2
adammertel <mertel.adam@gmail.com>
2018-10-29 08:15:47 +0100

    filter selection

---
+++ app/components/attribute/filterselection.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class AttributeFilterSelectionComponent extends React.Component {
+  props;
+  state;
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="panelfilter-wrapper panel-component panel-top-component">
+        <div className="first-line panel-top-component-line">
+          <div className="new-filter">
+            <div className="select is-small is-secondary">
+              <select onChange={this.props.handleNewFilterAdd}>
+                <option>Select attribute to filter</option>
+                {this.props.columnsNotUsed.map(column => {
+                  return (
+                    <option value={column.id} key={column.id}>
+                      {column.label}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- app/containers/attribute.tsx
+++ app/containers/attribute.tsx
@@ -3,8 +3,7 @@ import { observer } from 'mobx-react';
 import { SizeModel } from './../helpers/models';
 import Base from './../helpers/base';
 
-import AttributeSettingsComponent from './../components/attribute/settings';
-import AttributeFilterComponent from './../components/attribute/filter';
+import AttributeFilterSelectionComponent from './../components/attribute/filterselection';
 
 type Props = {
   stores: Array<Object>;
@@ -20,14 +19,20 @@ export default class AttributeContainer extends React.Component<Props> {
     super(props);
   }
 
+  handleNewFilterAdd(e): void {
+    const newColumn = e.target.value;
+    this.props.stores.filter.addNew(newColumn);
+  }
+
   render() {
     const appStore = this.props.stores.app;
 
     return (
       <div className="container container-attributes">
-        <AttributeSettingsComponent store={appStore} />
-        <AttributeFilterComponent stores={this.props.stores} />
-        Attribute
+        <AttributeFilterSelectionComponent
+          columnsNotUsed={this.props.stores.filter.columnsNotUsed}
+          handleNewFilterAdd={this.handleNewFilterAdd.bind(this)}
+        />
       </div>
     );
   }

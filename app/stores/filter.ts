bc3f351a7e77c1b517fed584c52644f1036364e1
adammertel <mertel.adam@gmail.com>
2018-11-02 18:18:14 +0100

    filter bars have ids

--- app/stores/filter.ts
+++ app/stores/filter.ts
@@ -86,12 +86,15 @@ export default class AppStore {
   initFilters(): void {
     const maxNumberOfIntervals = 10;
 
+    var newId = 0;
     const newBar = (label, checkFn) => {
+      newId = newId + 1;
       return {
         label: label,
         check: checkFn,
         occ: 0,
         active: true,
+        id: newId,
       };
     };
     this._columns.forEach(column => {
@@ -113,7 +116,7 @@ export default class AppStore {
 
         const barsSorted = barsAll.sort((a, b) => (a.occ > b.occ ? -1 : 1));
 
-        const others = { label: 'others', occ: 0 };
+        const others = newBar('others', () => true);
         barsSorted.forEach((f, fi) => {
           if (fi < maxNumberOfIntervals - 1) {
             bars.push(f);
@@ -152,78 +155,5 @@ export default class AppStore {
   }
 
   @action
-  addNew(columnId): number {
-    this._newId = this._newId + 1;
-    const newFilters = this.filters.slice();
-    if (!newFilters.find(f => f.column.id === columnId)) {
-      const column = this.columnById(columnId);
-      if (column) {
-        const newFilter = {
-          id: this._newId,
-          column: column,
-          values: [], //column['values'].slice()
-        };
-        console.log('newFilter', newFilter);
-        newFilters.push(newFilter);
-        this._filters.set(newFilters);
-      }
-    }
-    return this._newId;
-  }
-
-  @action
-  remove(filterId): void {
-    console.log('removing', filterId);
-    const newFilters = this.filters.slice().filter(f => f.id !== filterId);
-
-    if (this.filters.length !== newFilters) {
-      this._filters.set(newFilters);
-    }
-  }
-
-  @action
-  addValue(value, id): void {
-    const newFilters = this.filters.slice();
-    const filter = newFilters.find(f => f.id === id);
-    if (filter) {
-      if (!filter.values.includes(value)) {
-        filter.values.push(value);
-      }
-      this._filters.set(newFilters);
-    }
-  }
-
-  @action
-  toggleValue(value, id): void {
-    const newFilters = this.filters.slice();
-    const filter = newFilters.find(f => f.id === id);
-    if (filter) {
-      // removing value
-      if (filter.values.includes(value)) {
-        const index = filter.values.indexOf(value);
-        if (index > -1) {
-          filter.values.splice(index, 1);
-        }
-      }
-
-      // adding value
-      else {
-        filter.values.push(value);
-      }
-      this._filters.set(newFilters);
-    }
-  }
-
-  @action
-  removeValue(value, id): void {
-    const newFilters = this.filters.slice();
-    const filter = newFilters.find(f => f.id === id);
-    if (filter) {
-      const index = filter.values.indexOf(value);
-      if (index > -1) {
-        filter.values.splice(index, 1);
-        this._filters.set(newFilters);
-      }
-    }
-  }
+  toggleBar = (column, barLabel) => {};
 }
